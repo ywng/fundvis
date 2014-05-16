490d7620e90861ee6010fc838e4c1f435fc23a78
Jason Ng <ywng@ust.hk>
2014-05-17 02:06:46 +0800

    default: no special comment

--- php/api/application/controllers/fund.php
+++ php/api/application/controllers/fund.php
@@ -22,8 +22,8 @@ class Fund extends REST_Controller {
 
 	public function getAllFunds_get(){
 		$this->load->model('fund_model'); 
-		//$funds = $this->fund_model->getAllFunds();
-		//$this->core_controller->add_return_data('funds', $funds); 
+		$funds = $this->fund_model->getAllFunds();
+		$this->core_controller->add_return_data('funds', $funds); 
 		$this->core_controller->successfully_processed();
 
 	}
