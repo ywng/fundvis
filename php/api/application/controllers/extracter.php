38ffa7ba48232b0b43fdf10eee7ec4b4e99352f1
Jason Ng <ywng@ust.hk>
2014-05-17 22:55:57 +0800

    default: no special comment

--- php/api/application/controllers/extracter.php
+++ php/api/application/controllers/extracter.php
@@ -65,12 +65,11 @@ class Extracter extends REST_Controller {
 		$element_div=$html->find('div[class=daily_price_box]')[0];
 		$raw_str=$element_div->children(1)->children(0)->children(0)->plaintext;
 		$data=explode(" ",$raw_str);
-		for($i;$i<count($data);$i++){
-			if($data[$i]!=""){
-				$this->core_controller->add_return_data($i, $data[$i]); 
-			}
-		}
-		
+		$price=preg_replace("/[\t]/", '',$data[16]);
+		$date_str=preg_replace("/[\t]/", '',$data[2]);
+
+		$this->core_controller->add_return_data('fu', $price); 
+		$this->core_controller->add_return_data('funds', $date_str); 
 		$this->core_controller->successfully_processed();
 
 		$this->fund_model->insert_fund_daily_price($fund[$this->fund_model->KEY_fund_id],$raw_str,"2014-05-16");
