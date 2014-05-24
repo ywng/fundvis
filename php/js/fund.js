1be5b0adee010367b4e8978285ae91474bcf3df9
Jason Ng <ywng@ust.hk>
2014-05-25 00:39:24 +0800

    default: no special comment

--- php/js/fund.js
+++ php/js/fund.js
@@ -69,8 +69,8 @@ function _init_funds(){
 }
 
 function find_max_min_selected_funds(){
-	var max=parseFloat(funds[0].price_array[0].price);
-	var min=parseFloat(funds[0].price_array[0].price);
+	var max=-999999;
+	var min=999999;
 	for(var i=0;i<funds.length;i++){
 		if(funds[i].vis=="True"){
 			for(var j=0;j<funds[i].price_array.length;j++){
