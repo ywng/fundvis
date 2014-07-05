1ad67e2b2617ad4b1d874f27a485aae173e97cdd
Jason Ng <ywng@ust.hk>
2014-07-05 11:59:07 +0800

    default: no special comment

--- php/js/fund_vis.js
+++ php/js/fund_vis.js
@@ -1,7 +1,7 @@
   /**
   * Global Variables
   */  
-  var mode="percent";//actual price || percentage change
+  var mode="percent";//actual price || percentage change    mode
   var margin = {top: 15, right: 50, bottom: 100, left: 50},
       margin2 = {top: 515, right: 50, bottom: 50, left: 50},
       width = 1200 - margin.left - margin.right,
