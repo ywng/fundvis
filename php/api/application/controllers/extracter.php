ce5e733dee92e78e1b17a5cad33116b29c4d04db
Jason Ng <ywng@ust.hk>
2014-07-12 00:46:02 +0800

    add stock price extracter

--- php/api/application/controllers/extracter.php
+++ php/api/application/controllers/extracter.php
@@ -44,6 +44,50 @@ class Extracter extends REST_Controller {
 		$this->core_controller->successfully_processed();
 
 	}
+
+	public function stock_extract_get(){
+
+		$this->load->model('stock_model'); 
+		$stocks = $this->stock_model->get_all_stocks();
+
+		foreach ($stocks as $stock){
+
+			// Create DOM from URL or file
+			$html = file_get_html($stock[$this->stock_model->KEY_link]);
+			if (strlen(strstr($stock[$this->stock_model->KEY_link],"http://www.aastocks.com/"))>0) {
+				// AASTOCK webpages
+				$this->AASTOCK_stock_extract($html,$stock);
+
+			}
+
+		}
+
+		$this->core_controller->successfully_processed();
+
+	}
+
+	private function AASTOCK_stock_extract($html,$stock){
+		// price span
+		/*$price_e=$html->find('span[class=price]')[0];
+		
+		$price=preg_replace("/[^0-9.]/", '',$price_e->plaintext);
+
+		//date span
+		$date_e=$html->find('p[class=fine_print]')[0];
+		//var_dump($date_e);
+		if (preg_match('/(0[1-9]|1[012])[\/](0[1-9]|[12][0-9]|3[01])[\/](19|20)[0-9]{2}/',$date_e->plaintext, $regs)) {
+			$date_str = $regs[0];
+	    } 
+
+	    $fund_daily_price = array(
+               $this->fund_model->KEY_name => $fund[$this->fund_model->KEY_name],
+               $this->fund_model->KEY_price => $price ,
+               $this->fund_model->KEY_datetime => $date_str
+        );
+        $this->core_controller->add_return_data($fund[$this->fund_model->KEY_fund_id],$fund_daily_price); 
+
+	    $this->fund_model->insert_fund_daily_price($fund[$this->fund_model->KEY_fund_id],$price,$date_str);*/
+	}
 	
 
 	private function bloomberg_extract($html,$fund){
---
+++ php/api/application/models/stock_model.php
@@ -0,0 +1,99 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Funds
+*
+ * @author ngyikwai
+ */
+class Stock_model extends CI_Model{
+
+    var $KEY_stock_id = 'code';
+    var $KEY_name = 'name';
+    var $KEY_link = 'link';
+    var $KEY_remark = 'remark';
+    var $KEY_valid = 'valid';
+    var $Table_name_stock = 'Stock';
+
+
+    var $Table_name_price = 'StockPrice';
+    var $KEY_price_stock_id = 'sid';
+    var $KEY_price = 'price';
+    var $KEY_datetime = 'datetime';
+    
+
+    function __construct() {
+        parent::__construct();
+    }
+    
+    
+    /*
+     * 
+     */
+    /*public function get_max_date_with_id_array($requiredFundIDArray){
+        $this->db->or_where_in($this->KEY_price_fund_id,$requiredFundIDArray);
+        $this->db->select_max($this->KEY_datetime);
+        return $this->db->get($this->Table_name_price)->result();
+    }
+
+    public function get_min_date_with_id_array($requiredFundIDArray){
+        $this->db->or_where_in($this->KEY_price_fund_id,$requiredFundIDArray);
+        $this->db->select_min($this->KEY_datetime);
+        return $this->db->get($this->Table_name_price)->result();
+    }
+
+    public function get_max_date(){
+        $this->db->select_max($this->KEY_datetime);
+        return $this->db->get($this->Table_name_price)->result();
+    }
+
+    public function get_min_date(){
+        $this->db->select_min($this->KEY_datetime);
+        return $this->db->get($this->Table_name_price)->result();
+    }*/
+
+    public function get_all_stocks(){
+        $this->db->where($this->KEY_valid,1);
+        return $this->db->get($this->Table_name_stock)->result_array();
+    }
+
+/*
+    public function get_funds_with_id_array($requiredFundIDArray){
+        $this->db->or_where_in($this->KEY_fund_id,$requiredFundIDArray);
+        $this->db->where($this->KEY_valid,1);
+        return $this->db->get($this->Table_name_fund)->result_array();
+    }*/
+   
+
+    public function get_all_prices(){
+        $this->db->order_by($this->KEY_datetime, "desc"); 
+        return $this->db->get($this->Table_name_price)->result_array();
+    }
+
+    public function get_stock_price_by_id($id){
+        $this->db->order_by($this->KEY_datetime, "desc"); 
+        $this->db->select($this->KEY_price.",".$this->KEY_datetime);
+        $this->db->where($this->KEY_price_stock_id,$id);
+        return $this->db->get($this->Table_name_price)->result_array();
+    }
+
+    public function insert_stock_daily_price($id, $price,$date_str){
+        $date = new DateTime($date_str);
+        $this->db->where($this->KEY_price_stock_id,$id);
+        $this->db->where($this->KEY_datetime,date_format($date, 'Y-m-d H:i:s'));
+        $q = $this->db->get($this->Table_name_price);
+        if ( $q->num_rows() == 0 ) {
+            $data = array(
+               $this->KEY_price_stock_id => $id,
+               $this->KEY_price => $price ,
+               $this->KEY_datetime => date_format($date, 'Y-m-d H:i:s')
+            );
+
+            $this->db->insert($this->Table_name_price, $data); 
+        }
+
+    }
+    
+    
+}
+
+/* end of file stock_model.php */
