0dbf292b33d04ab4f49139454db4b4755f1b0fdc
Jason Ng <ywng@ust.hk>
2014-07-12 17:44:38 +0800

    default: no special comment

--- php/api/application/controllers/extracter.php
+++ php/api/application/controllers/extracter.php
@@ -73,16 +73,14 @@ class Extracter extends REST_Controller {
 		// price span
 		$price_e=$html->find('span[class=neg bold]')[0];
 		if(!$price_e){
-			$price_e=$html->find('span[class=pos bold]')[0];
-			$price_chg_e=$html->find('span[class=pos bold]')[1];
+			$price_e=$html->find('span[class=pos bold]')[2];
+			$price_chg_e=$html->find('span[class=pos bold]')[3];
 			$price_chg=(float)preg_replace("/[^0-9.]/", '',$price_chg_e->plaintext);
 			$price_chg=$price_chg*-1;
 
 		}else{
 			$price_chg_e=$html->find('span[class=neg bold]')[1];
 			$price_chg=(float)preg_replace("/[^0-9.]/", '',$price_chg_e->plaintext);
-
-
 		}
 		
 		$price=(float)preg_replace("/[^0-9.]/", '',$price_e->plaintext);
