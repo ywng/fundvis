6f8c8eff1d5ed803c1c72a92689ac4b2ef231baa
Jason Ng <ywng@ust.hk>
2014-07-13 12:36:33 +0800

    default: no special comment

--- .openshift/cron/minutely/extract.sh
+++ .openshift/cron/minutely/extract.sh
@@ -1,1 +1,8 @@
-curl http://fundvis-ywng.rhcloud.com/api/extracter/stock_extract
+#!/bin/bash
+
+minute=$(date +%M)
+if [[ $minute =~ .*[05] ]] then
+    curl http://fundvis-ywng.rhcloud.com/api/extracter/stock_extract 
+fi
+
+
