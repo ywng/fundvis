61cb509133f9b0031bd0de78d4db004a996dfa73
Jason Ng <ywng@ust.hk>
2014-07-14 23:44:26 +0800

    default: no special comment

---
+++ php/js/calls.ajax.js
@@ -0,0 +1,38 @@
+//var 
+var fblogin_URL='user/fblogin'
+
+
+function fblogin(access_token){
+	var onSuccess=function(data, textStatus, jqXHR){
+		 if(data.status_code=='1'){
+		 	localStorage.setItem("X-WealthVis-session-token", data.session_token);
+            localStorage.setItem("X-WealthVis-email", data.email);
+            localStorage.setItem("X-WealthVis-user-type",'user');
+            localStorage.setItem("X-WealthVis-loggedIn", "true");
+            localStorage.setItem("X-WealthVis-expire-time", data.expire_time);
+		 }
+	};
+	var on failure=function(jqHXR, textStatus, errorThrown){
+		 console.log('ajax error:' +textStatus + ' ' + errorThrown);
+	};
+	var data="access_token="+access_token;
+	rawAjaxCall(fblogin_URL,"POST",data,onSuccess,onFailure);
+}
+
+
+function rawAjaxCall(relativeURL,type,data,onSuccess,onFailure){
+
+	$.ajax({
+        url: serverDomain + relativeURL,
+        data:data,
+        headers : {"X-WealthVis-session-token":localStorage.getItem("X-WealthVis-session-token"),"X-WealthVis-email":localStorage.getItem("X-WealthVis-email"),"X-WealthVis-user-type":localStorage.getItem("X-WealthVis-user-type")},
+        type: type, 
+        success: onSuccess(data, textStatus, jqXHR),
+        error: onFailure(jqHXR, textStatus, errorThrown),
+
+    }); // end of the ajax call
+
+}
+
+
+ 
