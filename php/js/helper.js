e1bab5f6e9d304e5c723cfa4a8e31ce422fda1a8
Jason Ng <ywng@ust.hk>
2014-07-15 00:13:29 +0800

    default: no special comment

--- php/js/helper.js
+++ php/js/helper.js
@@ -1,14 +1,17 @@
 
 function checkLogin(){
-	if(!localStorage.getItem("X-WealthVis-loggedIn")||!localStorage.getItem("X-WealthVis-session-token")||localStorage.getItem("X-WealthVis-loggedIn")=="false"){//no session records, redirect to login
-		 if($(location).attr('href')!=(serverDomain + 'login.html'))
-			 $(location).attr('href', serverDomain + 'login.html');
-	}else if(localStorage.getItem("X-WealthVis-loggedIn")=="true"){
+	if(localStorage.getItem("X-WealthVis-loggedIn")=="true"){
 	   
 	    if(localStorage.getItem("X-WealthVis-user-type")=="user"){
 	        $(location).attr('href', serverDomain);
 	    } 
 
+	}
+}
+
+function checkRedirectNeeded(){
+	if(!localStorage.getItem("X-WealthVis-loggedIn")||!localStorage.getItem("X-WealthVis-session-token")||localStorage.getItem("X-WealthVis-loggedIn")=="false"){//no session records, redirect to login
+		$(location).attr('href', serverDomain + 'login.html');
 	}
 }
 
