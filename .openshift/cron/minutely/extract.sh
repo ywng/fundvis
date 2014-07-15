f97cbf874c2bfa1474d93870b58e6f60e7892fa6
Jason Ng <ywng@ust.hk>
2014-07-15 14:42:26 +0800

    .

--- .openshift/cron/minutely/extract.sh
+++ .openshift/cron/minutely/extract.sh
@@ -2,8 +2,9 @@
 if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
 	touch $OPENSHIFT_DATA_DIR/last_run
 fi
-if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +2) ]]; then #run every 3 mins
+if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +1) ]]; then #run every 2 mins
 	rm -f $OPENSHIFT_DATA_DIR/last_run
 	touch $OPENSHIFT_DATA_DIR/last_run
-	curl http://fundvis-ywng.rhcloud.com/api/extracter/stock_extract 
+	curl http://fundvis-ywng.rhcloud.com/api/extracter/stock_extract_batch1 
+	curl http://fundvis-ywng.rhcloud.com/api/extracter/stock_extract_batch2
 fi  
