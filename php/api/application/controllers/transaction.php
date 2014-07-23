5b9136a396136eb11ca91d0e8b1362fceb9a19ea
Jason Ng <ywng@ust.hk>
2014-07-23 19:10:18 +0800

    default: no special comment

--- php/api/application/controllers/transaction.php
+++ php/api/application/controllers/transaction.php
@@ -98,9 +98,10 @@ class Transaction extends REST_Controller {
 
 	private function get_all_open_buys_quantity($stock_id,$uid){
 		$open_buys=$this->transaction_model->get_all_open_buy_record_with_uid_sid($stock_id,$uid);
-		$quantity=-1;
+		
 		if(count($open_buys)<1)
-			return $quantity;
+			return -1;
+		$quantity=0;
 		foreach($open_buys as $buy){
 			$quantity+=$buy[$this->transaction_model->KEY_quantity];
 		}
