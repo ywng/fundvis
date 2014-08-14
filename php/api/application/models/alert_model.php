7e9f665793038d703319b05d04df1d395748715a
Jason Ng <ywng@ust.hk>
2014-08-14 23:38:34 +0800

    default: no special comment

--- php/api/application/models/alert_model.php
+++ php/api/application/models/alert_model.php
@@ -48,7 +48,7 @@ class Alert_model extends CI_Model{
 
    public function get_all_alerts_by_uid($uid){
        $this->db->select('*');
-       $this->db->join($this->stock_model->Table_name_stock, $this->stock_model->Table_name_stock.'\.'.$this->stock_model->KEY_stock_id= $this->Table_name_alert.'\.'.$this->alert_model->KEY_stock_id);
+       $this->db->join($this->stock_model->Table_name_stock, $this->stock_model->Table_name_stock.'.'.$this->stock_model->KEY_stock_id .'='. $this->Table_name_alert.'.'.$this->alert_model->KEY_stock_id);
 
        $this->db->where($this->KEY_user_id,$uid);
        $q=$this->db->get($this->Table_name_alert);
