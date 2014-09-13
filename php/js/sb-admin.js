d2154b1247a89cc7ad8597be8ad9fbccf091c7f4
Jason Ng <ywng@ust.hk>
2014-09-13 14:34:35 +0800

    default: no special comment

--- php/js/sb-admin.js
+++ php/js/sb-admin.js
@@ -16,3 +16,17 @@ $(function() {
         }
     })
 })
+
+
+function sideBarInit(){
+	$('#side-menu').metisMenu();
+
+	$(window).bind("load resize", function() {
+        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.sidebar-collapse').addClass('collapse')
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse')
+        }
+    })
+}
