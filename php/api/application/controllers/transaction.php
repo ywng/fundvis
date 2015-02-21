30eb0f5d22b96ebfcf63643a03e91f05ea3b8174
Jason Ng <ywng@ust.hk>
2015-02-21 17:05:13 +0800

    default: no special comment

--- php/api/application/controllers/transaction.php
+++ php/api/application/controllers/transaction.php
@@ -37,7 +37,7 @@ class Transaction extends REST_Controller {
 		$user=$this->core_controller->get_current_user();
 		$uid=$user[$this->user_model->KEY_user_id];
 
-		$user_trans_record=$this->get_all_trans_record_by_uid($uid);
+		$user_trans_record=$this->transaction_model->get_all_trans_record_by_uid($uid);
 		$this->core_controller->add_return_data('user_trans_record', $user_trans_record); 
 
 		$this->core_controller->successfully_processed();
