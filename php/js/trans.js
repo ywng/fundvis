41296e95ed8a16e63e9510d4bf994fd2479ae814
Jason Ng <ywng@ust.hk>
2015-02-21 17:47:35 +0800

    default: no special comment

--- php/js/trans.js
+++ php/js/trans.js
@@ -55,13 +55,25 @@ function transDataTableInit(){
                  var transaction=new Array(); 
                  transaction[0]=data.user_trans_record[i].id;
                  transaction[1]="<div class=\"tooltip-demo\"><input type=\"checkbox\" id=\"alert"+data.user_trans_record[i].id+"\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Transaction id:"+data.user_trans_record[i].id+"\"></input></div>";
-                 transaction[2]= "<div class=\"tooltip-demo\"><button type=\"button\" class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title=\""+data.user_trans_record[i].name+"\" style=\"width: 50px; height: 23px; padding:1px 5px;\">"+data.user_trans_record[i].sid+"</button></div>";
+                 transaction[2]="<div class=\"tooltip-demo\"><button type=\"button\" class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title=\""+data.user_trans_record[i].name+"\" style=\"width: 50px; height: 23px; padding:1px 5px;\" href=\""+data.user_trans_record[i].link+"\">"+data.user_trans_record[i].sid+"</button></div>";
+                 transaction[3]=data.user_trans_record[i].type;
+                 transaction[4]=data.user_trans_record[i].price;
+                 transaction[5]=data.user_trans_record[i].target_price;
+                 transaction[6]=data.user_trans_record[i].target_price_renotify_percent;
+                 transaction[7]=data.user_trans_record[i].stop_loss_price;
+                 transaction[8]=data.user_trans_record[i].stop_loss_price_renotify_percent;
+                 transaction[9]=data.user_trans_record[i].quantity;
+                 transaction[10]=data.user_trans_record[i].trans_fee;
+                 transaction[11]=data.user_trans_record[i].datetime;
+                 transaction[12]=data.user_trans_record[i].rationale;
+                 transaction[13]=data.user_trans_record[i].review;
+                 transaction[14]=data.user_trans_record[i].closed_quantity;
+                 transaction[15]=data.user_trans_record[i].closed_price;
 
-                 
                  //========hidden fields=========================//
                  //alert id , not displayed but use as row id for matching 
                  
-                 transaction[16]="old";//identify the alerts are old record, not newly added
+                 transaction[20]="old";//identify the alerts are old record, not newly added
              
                  //data for sort
             
@@ -80,6 +92,68 @@ function transDataTableInit(){
     getUserTransRecord(loadUserTransOnSuccess);
 
 
+}
+
+function doTableRefresh(table,data) {
+    table.fnClearTable();
+    table.fnAddData(data);
+    tableMakeEditable(table);
+    table.fnDraw();
+}
+
+function doTableUISetUp(){
+    $('.tooltip-demo').tooltip({
+        selector: "[data-toggle=tooltip]",
+        container: "body"
+    });
+}
+
+function tableMakeEditable(table){
+    table.makeEditable({
+        //...
+        sUpdateURL: function(value, settings){
+            var rowId = table.fnGetPosition(this)[0];
+            var columnPosition = table.fnGetPosition(this)[1];
+            var aData = table.fnGetData(rowId);
+            console.log(rowId+"  "+columnPosition);
+
+            var formdata = new FormData();
+           /*formdata.append( 'alert_id',aData[0]);
+            if(columnPosition==4){
+                var alert_type=$( '#type-'+aData[0]+' option:selected').val();
+                var formdata2 = new FormData();
+                formdata2.append( 'alert_id',aData[0]);
+                formdata2.append( 'field',"type");
+                formdata2.append( 'value',alert_type);
+                updateAlert(formdata2,null);//first reset the alert type first
+
+                if(alert_type=="3"||alert_type=="4"){
+                    formdata.append( 'field',"specified_price");
+                }else if(alert_type=="5"||alert_type=="6"){
+                    formdata.append( 'field',"daily_percent");
+                }
+
+            }else if (columnPosition==5){
+                formdata.append( 'field',"renotify_diff_percent");
+            }
+            formdata.append( 'value',value);
+
+            updateAlert(formdata,null);*/
+
+            return value
+        },
+        "aoColumns": [
+            null,
+            null,
+            null,
+            null,
+            {}, 
+            {},
+            null,
+            null
+        ]
+   
+    }); 
 }
 /*
  * End of Transactions DataTable Related
