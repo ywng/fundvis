04e88e85e81ec1b7edfac93dff0b27594e8f47e6
Jason Ng <ywng@ust.hk>
2015-02-21 21:24:50 +0800

    default: no special comment

--- php/js/alert.js
+++ php/js/alert.js
@@ -44,7 +44,7 @@ function alertDataTableInit(){
         },
         "fnDrawCallback": function(){
             doTableUISetUp();
-        } 
+        }, 
         "bAutoWidth": false
     });
     tableMakeEditable(alertsTable);
