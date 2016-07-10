73b285ed68618f8125276633dc48bf41e02f4277
Jason Ng <ywng@ust.hk>
2016-07-10 15:15:20 +0800

    default: no special comment

--- .openshift/cron/daily/extract.sh
+++ .openshift/cron/daily/extract.sh
@@ -1,2 +1,8 @@
 curl http://fundvis-ywng.rhcloud.com/api/extracter/AASTOCK_fetch_stock_name_Chinese
-curl http://mpf.vis.ywng.cloudbees.net/GetUnitPrice
+
+#stock price extract
+curl http://fundvis-ywng.rhcloud.com/api/extracter/stock_extract_batch1 
+curl http://fundvis-ywng.rhcloud.com/api/extracter/stock_extract_batch2
+curl http://fundvis-ywng.rhcloud.com/api/extracter/stock_extract_batch3
+curl http://fundvis-ywng.rhcloud.com/api/notification/routine_trans_target_stop_loss_check
+curl http://fundvis-ywng.rhcloud.com/api/notification/alert_check
--- .openshift/cron/hourly/extract.sh
+++ .openshift/cron/hourly/extract.sh
@@ -1,2 +1,2 @@
 curl http://fundvis-ywng.rhcloud.com/api/extracter/extract
-curl http://mpf.vis.ywng.cloudbees.net/GetUnitPrice
+
